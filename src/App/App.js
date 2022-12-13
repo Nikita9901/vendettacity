b806477de850c198d2913f805fd0ceff8a024f5f
nikita9901 <nikitichsn2@gmail.com>
2022-12-13 03:18:25 +0300

    init: create project and setup files

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>Hello</div>
+      <div className={styles.container}>Hello</div>
+      <div className={styles.container}>Hello</div>
+    </div>
+  );
+}
+
+export default App;
